0dd2c9fbaa907615342d111d5264eab440cdd8ed
PRINCE KUMAR <prince26460@gmail.com>
2025-04-10 22:06:16 +0530

    Add Mentor model implementation.

--- src/models/mentorship.model.js
+++ src/models/mentorship.model.js
@@ -1,1 +1,29 @@
+import mongoose, {Schema} from "mongoose";
 
+const mentorSchema = new Schema(
+    {
+        mentor:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        },
+        hasAvailability:{
+            type: Boolean,
+            default:false
+        },
+        skillTags: {
+            type: String,
+            required: true,
+        },
+        totalStudents: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref:"Student"
+            }
+        ]
+    }, 
+    {
+        timestamps: true
+    }
+);
+
+export const Mentor = mongoose.model('Mentor', mentorSchema);
